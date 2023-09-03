2e4279b7dc25ea27d53c2d53b66ffa6bf980dcdc
unknown <faculty199568@gmail.com>
2023-09-03 16:14:01 +0100

    task javascript warm-up is done

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,11 +1,11 @@
-let numbers = [4, 8, 15, 16, 12, 42];
+const numbers = [4, 8, 15, 16, 12, 42];
 
 // Find the index of the value 12
 const index = numbers.indexOf(12);
 
 // Replace 12 with 89 if found
 if (index !== -1) {
-    numbers[index] = 89;
+  numbers[index] = 89;
 }
 
 console.log(numbers); // Outputs [4, 8, 15, 16, 23, 42] with 12 replaced by 89
