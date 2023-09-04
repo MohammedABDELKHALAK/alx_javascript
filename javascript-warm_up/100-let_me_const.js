eed43fe355ffecbd02a3feaf8adceaddec6b0ce6
unknown <faculty199568@gmail.com>
2023-09-04 02:46:22 +0100

    task javascript warm-up is done

--- javascript-warm_up/100-let_me_const.js
+++ javascript-warm_up/100-let_me_const.js
@@ -1,13 +1,4 @@
-#!/usr/bin/node
-
-// Define the initial value of myVar
-let myVar = 123;
-
 // Modify the value of myVar to 333
 myVar = 333;
-
-// Log the modified value of myVar
-console.log(myVar);
-
 // Export the add function so it can be used in other modules
-module.exports
+module.exports.myVar
--- javascript-warm_up/100-main.js
+++ javascript-warm_up/100-main.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-myVar = 40;
+myVar = 89;
+require('./100-let_me_const')
 console.log(myVar);
-myVar = require('./100-let_me_const')
