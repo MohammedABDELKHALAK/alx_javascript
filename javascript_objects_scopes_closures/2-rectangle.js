46607ba0e8f781b1c6fb18835acf2066a49bf0b3
unknown <faculty199568@gmail.com>
2023-09-06 21:54:10 +0100

    task done JavaScript - Objects, Scopes and Closures

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -4,10 +4,12 @@ class Rectangle {
     constructor(w, h) {
       if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
  // If w or h is <= 0 or not a positive integer, set the toString method
-        this.toString = function () {
-            return 'Rectangle {}';
-        };
-        } else {
+          return 'Rectangle {}';
+        } 
+        else if(w === undefined || h === undefined){
+          return {};
+        }
+      else {
                 this.width = w;
                 this.height = h;
             }
