4dead8a343fee987806f47dc8c8010e121297636
unknown <faculty199568@gmail.com>
2023-09-10 21:39:58 +0100

    javascript network is done!

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,44 +1,38 @@
 const request = require('request');
 
-// Get the movie ID from the command line arguments
-const movieId = process.argv[2];
+const movieId = process.argv[2]; // Get the movie ID from the command line arguments
 
 if (!movieId) {
-  console.error('Please provide a movie ID as an argument.');
+  console.log('Please provide a valid movie ID.');
   process.exit(1);
 }
 
-// Define the Star Wars API endpoint
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-// Make a GET request to the API URL
-request.get(apiUrl, (error, response, body) => {
+request(apiUrl, (error, response, body) => {
   if (error) {
-    console.error('Error:', error.message);
-    process.exit(1);
+    console.error('Error:', error);
+    return;
   }
 
   if (response.statusCode !== 200) {
-    console.error('HTTP request failed with status code:', response.statusCode);
-    process.exit(1);
+    console.error(`Request failed with status code ${response.statusCode}`);
+    return;
   }
 
-  // Parse the JSON response
-  const movie = JSON.parse(body);
+  const movieData = JSON.parse(body);
+  const characters = movieData.characters;
 
-  // Print the characters
-  movie.characters.forEach((characterUrl) => {
-    // Make a GET request to the character URL
-    request.get(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error:', charError.message);
-      } else if (charResponse.statusCode !== 200) {
-        console.error('HTTP request failed with status code:', charResponse.statusCode);
-      } else {
-        // Parse the character JSON response
-        const character = JSON.parse(charBody);
-        console.log(character.name);
-      }
+  if (characters.length === 0) {
+    console.log('No characters found for this movie.');
+  } else {
+    characters.forEach((characterUrl) => {
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (!charError && charResponse.statusCode === 200) {
+          const characterData = JSON.parse(charBody);
+          console.log(characterData.name);
+        }
+      });
     });
-  });
+  }
 });
